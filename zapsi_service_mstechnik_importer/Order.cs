a2d3da44d95040d72fa3876f612a7d1033d80f18
Petr Jahoda <jahoda@zapsi.eu>
2019-06-11 07:49:32 +0200

    Initial commit
    Took 6 minutes

---
+++ zapsi_service_mstechnik_importer/Order.cs
@@ -0,0 +1,9 @@
+namespace zapsi_service_optimont_importer {
+    public class Order {
+        public int Oid { get; set; }
+        public string WorkplaceId { get; set; }
+        public string ProductId { get; set; }
+        public string RequestedAmount { get; set; }
+        public string Barcode { get; set; }
+    }
+}
---
+++ zapsi_service_mstechnik_importer/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace zapsi_service_mstechnik_importer {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
